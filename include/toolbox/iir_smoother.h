6eeaec35e6bcdb98854bcd5b6d4a8b3b96030c64
Lawrence Ibarria <lawrence@verdantrobotics.com>
2019-08-18 14:47:46 -0700

    Add iir_smoother

---
+++ include/toolbox/iir_smoother.h
@@ -0,0 +1,25 @@
+#pragma once
+
+/// simple infinte impulse response filter
+struct iir_smoother
+{
+    double val_;
+    double alpha_;
+
+    iir_smoother( double v = 0, double alpha = 0.5 )
+    {
+        val_ = v;
+        alpha_ = alpha;
+    }
+
+    double update( double v )
+    {
+        val_ = alpha_*v + (1.0f-alpha_)*val_;
+        return val_;
+    }
+
+    double operator()( double v )
+    {
+        return update(v);
+    }
+};
