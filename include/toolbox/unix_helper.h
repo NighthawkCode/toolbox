cc283fc54a715011824f2ed8189e9067e33277b3
Hasan Tafish <hasan.tafish@verdantrobotics.com>
2020-04-09 09:25:18 -0700

    Add a helper function to get char without pressing Enter

--- include/toolbox/unix_helper.h
+++ include/toolbox/unix_helper.h
@@ -3,3 +3,8 @@
 // Execute a command; output is returned in 'output'.  Returns true if
 // successful, false if not.
 bool exec(const char* cmd, std::string &output);
+
+/// get a character from stdin byte by byte
+/// This is useful if we don't want to wait 
+/// for the user to hit enter to process stdin
+char getchar_non_canonical();
