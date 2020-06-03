9768769907a86d1646ecd441f53744e44b5024ec
Paul Miller <paul.miller@verdantrobotics.com>
2020-06-03 11:32:29 -0700

    move epoch_to_sql_time() to here

---
+++ src/time_format.cpp
@@ -0,0 +1,15 @@
+#include "time_format.h"
+
+
+std::string epoch_to_sql_time(double t)
+{
+  double fracpart, intpart;
+  fracpart = std::modf(t, &intpart);
+  time_t epoch = (time_t)intpart;
+  struct tm *tm = gmtime(&epoch);
+  char outstr[100];
+  strftime(outstr, sizeof(outstr), "%F %T", tm);
+  std::string res = outstr;
+  res += "." + std::to_string(int(fracpart*10000));
+  return res;
+}
